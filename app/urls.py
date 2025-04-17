6da41424db27d9c03b4d7be3af8772d8f83454e0
Katie Bridson <kbridson@memphis.edu>
2025-04-17 13:32:26 -0500

    Add user details and links

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ app_name = "app"
 
 urlpatterns = [
     path("users/", views.user_list, name="user_list"),
+    path("users/<int:id>", views.user_details, name="user_details"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import User
 
 
@@ -6,3 +6,8 @@ from .models import User
 def user_list(request):
     users = User.objects.all()
     return render(request, "users/user_list.html", {"users": users})
+
+
+def user_details(request, id):
+    user = get_object_or_404(User, id=id)
+    return render(request, "users/user_details.html", {"user": user})
