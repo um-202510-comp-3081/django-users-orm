c088bb2a3f546c219adf50844b830907c263bcb2
Katie Bridson <kbridson@memphis.edu>
2025-04-17 12:28:53 -0500

    Added validations

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField()
-    email = models.EmailField()
-    age = models.PositiveIntegerField()
-    ssn = models.CharField()
+    name = models.CharField(max_length=300, validators=[MinLengthValidator(2)])
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField(blank=True)
+    ssn = models.CharField(unique=True)
